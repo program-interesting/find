af8b80797fb6cb3f3d82420ad9573a57dede9c03
lidi3@tal100.com <woailidi>
2020-03-27 17:25:29 +0800

    add CRos

--- service/api.js
+++ service/api.js
@@ -32,6 +32,17 @@ exp.use(bodyParser.json({limit: '20mb'}));
 exp.use(bodyParser.urlencoded({limit: '20mb', extended: false}));
 exp.use(bodyParser.text());
 /**
+ * 解决跨域问题
+ */
+exp.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",'Find v0.0.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+/**
  * 载入：查询SQL方法
  */
 require('../service/select.js');
